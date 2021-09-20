34662f08d002e278fb722bde0e19d2da26235000
ryguy4125 <ryan.a.revilla@gmail.com>
2021-09-19 18:18:10 -0700

    resource picker implemented

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import { EmptyState, Heading, Layout, Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import { useState } from "react";
 
-const Index = () => (
+function Index() {
+  const [modal, setModal] = useState({open: false});
+
+  function handleSelection(resources) {
+    const getIdFromResources = resources.selection.map((product) => product.id);
+    console.log(getIdFromResources);
+  }
+
+  return(
   <Page>
     <Layout>
+      <ResourcePicker 
+        resourceType="Product"
+        showVariants={false}
+        open={modal.open}
+        onSelection={(resources) => handleSelection(resources)}
+        onCancel={() => setModal({open:false})}
+      />
       <EmptyState
-        heading="Manage your inventory transfers"
-        action={{content: 'Add transfer'}}
+        heading="Select Products"
+        action={{
+          content: 'SelectProduct',
+          onAction: () => setModal({open:true})
+        }}
         secondaryAction={{content: 'Learn more', url: 'https://help.shopify.com'}}
         image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
         >
@@ -13,6 +33,6 @@ const Index = () => (
       </EmptyState>
     </Layout>
   </Page>
-);
+  )};
 
 export default Index;
