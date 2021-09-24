a7b84890486373b860ed4aa6f1d19f9244e3fedb
ryguy4125 <ryan.a.revilla@gmail.com>
2021-09-23 20:22:00 -0700

    script tags input

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,3 +1,48 @@
+import gql from "graphql-tag";
+
+
+const CREATE_SCRIPT_TAG = gql`
+    mutation scriptTagCreate($input: ScriptTagInput!) {
+        scriptTagCreate(input: $input) {
+            scriptTag {
+                id
+            }
+            userErrors {
+                field
+                message
+            }
+        }
+    }
+`;
+
+const QUERY_SCRIPT_TAG = gql`
+query getScriptTags {
+    scriptTags(first: 5) {
+      edges {
+        node {
+          id
+          src
+          displayScope
+        }
+      }
+    }
+  }
+`;
+
+const DELETE_SCRIPT_TAG = gql`
+mutation deleteScriptTag($id: ID!){
+    scriptTagDelete(id: $id){
+      deletedScriptTagId
+      userErrors{
+        field
+        message
+      }
+    }
+  }
+`;
+
+
+
 function ScriptPage() {
     return (
         <div>
