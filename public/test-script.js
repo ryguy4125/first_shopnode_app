57302d24fc55462a78a3320f98d506ce24f218ef
ryguy4125 <ryan.a.revilla@gmail.com>
2021-09-23 21:54:40 -0700

    imported JQuery and wrapped scripts. Orange header bar.

--- public/test-script.js
+++ public/test-script.js
@@ -1,3 +1,12 @@
-console.log("success!!")
+const script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+script.type = "text/javascript";
+script.onreadystatechange = handler;
+script.onload = handler;
+document.getElementsByTagName("head")[0].appendChild(script);
 
-hea
+console.log("success!!")
+function handler() {
+    const header = $('#shopify-section-announcement-bar');
+    header.prepend('<div>Great Stuff Here!</div>').css({'background-color': 'orange', 'text-align': 'center'})
+}
